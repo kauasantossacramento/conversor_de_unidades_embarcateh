15d240b56049846da25bc31333f019b90616b684
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:42:07 -0300

    Criando arquivo .h para integração no main

---
+++ Conversor_Volume.h
@@ -0,0 +1,10 @@
+#ifndef VOLUME_H
+#define VOLUME_H
+
+// Função para converter volume entre diferentes unidades
+void converterVolume(float valor, int unidadeOrigem, int unidadeDestino);
+
+// Função principal que encapsula a lógica do conversor de volume
+void volume();
+
+#endif // VOLUME_H
