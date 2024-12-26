d5629a20b91c2de47dff52accebfd4c291026d32
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:47:26 -0300

    Criando arquivo .h para implementação no main

---
+++ Conversor_Velocidade_Potencia.h
@@ -0,0 +1,10 @@
+#ifndef CONVERSORES_H
+#define CONVERSORES_H
+
+// Função para converter unidades de velocidade
+void converterVelocidade();
+
+// Função para converter potência entre Watt, Kilowatt e Horsepower
+void converterPotencia();
+
+#endif // CONVERSORES_H
