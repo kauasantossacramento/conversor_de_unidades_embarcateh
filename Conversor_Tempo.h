f82caee7857996c1c24a43b5270702c3d9e7252a
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:37:18 -0300

    Arquivo .h para integração com main

---
+++ Conversor_Tempo.h
@@ -0,0 +1,7 @@
+#ifndef TEMPO_H
+#define TEMPO_H
+
+// Função para converter tempo entre diferentes unidades
+void converterTempo(float valor, int unidadeOrigem, int unidadeDestino);
+
+#endif // TEMPO_H
