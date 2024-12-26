65777f4b04467b5ef16d89921b1ab7638222bd22
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:28:25 -0300

    Criando arquivo .h do conversor de temperatura para integração no main

---
+++ Conversor_Temperatura.h
@@ -0,0 +1,10 @@
+#ifndef TEMPERATURA_H
+#define TEMPERATURA_H
+
+// Função para converter temperatura entre diferentes unidades
+void converterTemperatura(float valor, int unidadeOrigem, int unidadeDestino);
+
+// Função para gerenciar a lógica do conversor de temperatura
+void temperatura();
+
+#endif // TEMPERATURA_H
