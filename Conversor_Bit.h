88573f344f48e47f53d276d053425780e8a89e10
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:54:32 -0300

    Arquivo .h para implentação no main

---
+++ Conversor_Bit.h
@@ -0,0 +1,8 @@
+#ifndef CONVERSOR_BIT_H
+#define CONVERSOR_BIT_H
+
+// Função para realizar conversões entre diferentes unidades de armazenamento
+// de dados (KB, MB, GB, TB).
+int conversorBit();
+
+#endif // CONVERSOR_BIT_H
