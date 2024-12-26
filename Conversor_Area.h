c38c793ce451058260c363a252d4dcad0bfdce1f
kauasantossacramento <kauasantossacramento@gmail.com>
2024-12-26 19:59:06 -0300

    Arquivo .h para  implementar no arquivo main

---
+++ Conversor_Area.h
@@ -0,0 +1,8 @@
+#ifndef CONVERSOR_AREA_H
+#define CONVERSOR_AREA_H
+
+// Função para realizar a conversão de área entre metros quadrados e centímetros quadrados.
+// Retorna o valor convertido ou -1 se a escolha for inválida.
+double converterArea(int escolha, double area);
+
+#endif // CONVERSOR_AREA_H
